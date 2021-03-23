355752a1478e5bc00aa39cdc712ae578d858d2ae
Kathryn Kane <Kathryn@Kathryns-MacBook-Pro-2.local>
2021-03-22 20:58:56 -0700

    deliverable 1

--- static/js/app.js
+++ static/js/app.js
@@ -58,10 +58,10 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    for (let filter in filters) {
-      filteredData = filteredData.filter(row => row.filter === filter);
-    };
-  
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+    
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData)
   };
